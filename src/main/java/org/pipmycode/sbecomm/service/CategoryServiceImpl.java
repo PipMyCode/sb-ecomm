c252f015dd6a74108a64bbc8aabc3179e424ba2e
PipMyCode <adigunmarcus@gmail.com>
2026-05-19 09:22:43 -0400

    Implement CategoryServiceImpl with basic CRUD operations

---
+++ src/main/java/org/pipmycode/sbecomm/service/CategoryServiceImpl.java
@@ -0,0 +1,37 @@
+package org.pipmycode.sbecomm.service;
+
+import org.pipmycode.sbecomm.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+
+    //just a fake database
+    private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
+        categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst()
+                .get();
+
+        categories.remove(category);
+        return "Category with categoryId: " + categoryId + " deleted successfully!";
+    }
+}
