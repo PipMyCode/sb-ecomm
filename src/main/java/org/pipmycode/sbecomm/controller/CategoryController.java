330bd83303e8cf6e2931810294ee0c9f8fc0aed3
PipMyCode <adigunmarcus@gmail.com>
2026-05-19 09:22:30 -0400

    Add CategoryService and refactor CategoryController to use service layer

--- src/main/java/org/pipmycode/sbecomm/controller/CategoryController.java
+++ src/main/java/org/pipmycode/sbecomm/controller/CategoryController.java
@@ -1,19 +1,36 @@
 package org.pipmycode.sbecomm.controller;
 
 import org.pipmycode.sbecomm.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.pipmycode.sbecomm.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
 public class CategoryController {
 
-    private List<Category> categories = new ArrayList<>();
 
-    @GetMapping("api/public/categories")
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "Category added successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 }
---
+++ src/main/java/org/pipmycode/sbecomm/service/CategoryService.java
@@ -0,0 +1,12 @@
+package org.pipmycode.sbecomm.service;
+
+import org.pipmycode.sbecomm.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
+}
