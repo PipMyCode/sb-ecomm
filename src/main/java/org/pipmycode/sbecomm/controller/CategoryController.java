2a4c262b78f85d7ba05e1a43eb660cd13d8989e4
PipMyCode <adigunmarcus@gmail.com>
2026-05-20 01:23:03 +0100

    Enhance deleteCategory endpoint with proper HTTP response handling and exception handling in CategoryServiceImpl

--- src/main/java/org/pipmycode/sbecomm/controller/CategoryController.java
+++ src/main/java/org/pipmycode/sbecomm/controller/CategoryController.java
@@ -2,8 +2,10 @@ package org.pipmycode.sbecomm.controller;
 
 import org.pipmycode.sbecomm.model.Category;
 import org.pipmycode.sbecomm.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -29,8 +31,12 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId) {
-        String status = categoryService.deleteCategory(categoryId);
-        return status;
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
     }
 }
--- src/main/java/org/pipmycode/sbecomm/service/CategoryServiceImpl.java
+++ src/main/java/org/pipmycode/sbecomm/service/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package org.pipmycode.sbecomm.service;
 
 import org.pipmycode.sbecomm.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,9 +31,8 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
-                .orElse(null);
-        if (category == null)
-            return "Category not found";
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+
 
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " deleted successfully!";
