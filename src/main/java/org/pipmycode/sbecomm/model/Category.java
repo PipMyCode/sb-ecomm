a563bb333675b885d1c6916c859c6eb0ce63296e
PipMyCode <adigunmarcus@gmail.com>
2026-05-21 13:51:39 +0100

    Validate `categoryName` minimum length, handle duplicate category creation with ResourceAlreadyExistsException, and refactor exception handling in CategoryServiceImpl.

--- src/main/java/org/pipmycode/sbecomm/model/Category.java
+++ src/main/java/org/pipmycode/sbecomm/model/Category.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,5 +21,8 @@ public class Category {
     private Long categoryId;
 
     @NotBlank
+    @Size(min =5, message = "Category name must be at least 5 characters long")
     private String categoryName;
+
+
 }
--- src/main/java/org/pipmycode/sbecomm/service/CategoryServiceImpl.java
+++ src/main/java/org/pipmycode/sbecomm/service/CategoryServiceImpl.java
@@ -1,13 +1,13 @@
 package org.pipmycode.sbecomm.service;
 
+import org.pipmycode.sbecomm.exceptions.ResourceAlreadyExistsException;
 import org.pipmycode.sbecomm.model.Category;
 import org.pipmycode.sbecomm.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.pipmycode.sbecomm.exceptions.ResourceNotFoundException;
+import org.pipmycode.sbecomm.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -26,13 +26,18 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void createCategory(Category category) {
+        Category existingCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+
+        if (existingCategory != null) {
+            throw new ResourceAlreadyExistsException("Category with name: " + category.getCategoryName() + " already exists!");
+        }
         categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
         Category existingCategory = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResourceNotFoundException("Category with categoryId: " + categoryId + " not found!"));
         categoryRepository.delete(existingCategory);
         return "Category with categoryId: " + categoryId + " deleted successfully!";
     }
@@ -40,7 +45,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public String updateCategory(Category category, Long categoryId) {
         Category existingCategory = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResourceNotFoundException("Category with categoryId: " + categoryId + " not found!"));
 
         existingCategory.setCategoryName(category.getCategoryName());
         categoryRepository.save(existingCategory);
