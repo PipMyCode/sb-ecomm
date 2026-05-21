580a1b1f83f1685b864535f5217a1d0916fa1037
PipMyCode <adigunmarcus@gmail.com>
2026-05-21 13:51:06 +0100

    Add custom exceptions: ResourceAlreadyExistsException and ResourceNotFoundException

---
+++ src/main/java/org/pipmycode/sbecomm/exceptions/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package org.pipmycode.sbecomm.exceptions;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/pipmycode/sbecomm/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package org.pipmycode.sbecomm.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
